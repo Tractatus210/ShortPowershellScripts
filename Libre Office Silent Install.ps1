7a21f6f0a652445ead79cf62b075908e0790ce6e
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2023-01-23 22:50:18 +0000

    Add files via upload
    Silently installs Libre office on Windows devices.

---
+++ Libre Office Silent Install.ps1
@@ -0,0 +1,5 @@
+﻿$downloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$path = $downloads + "\libre.msi"
+Invoke-WebRequest -Uri "https://download.documentfoundation.org/libreoffice/stable/7.4.4/win/x86_64/LibreOffice_7.4.4_Win_x64.msi" -OutFile $path
+
+Install-Package $path
