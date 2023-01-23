83567cfc6dd6372ecf8f723cdb2424240f62e2c5
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2023-01-23 22:54:37 +0000

    Add files via upload
    Checks if desktop shortcuts for Google Chrome and Microsoft Edge exist and adds/removes them based on user input.

---
+++ Chrome Edge Desktop.ps1
@@ -0,0 +1,27 @@
+﻿$user = $env:UserName
+$desktopPath = -join("C:\Users\",$user,"\Desktop")
+$desktopContents = Get-ChildItem -Path $desktopPath -Name
+$chrome = "Google Chrome.lnk"
+$chromeDesk = "$desktopPath\$chrome"
+$edge = "Microsoft Edge.lnk"
+$edgeDesk = "$desktopPath\$edge"
+$chromeSh = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk"
+$edgeSh = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
+
+
+if ($desktopContents.Contains($chrome)){
+    $input = Read-Host "There is a Chrome desktop shortcut. Do you want to remove it? Y or N"
+    if ($input -eq "Y"){Remove-Item -Path $chromeDesk}
+    }else{
+        $input = Read-Host "There is no Chrome desktop shortcut. Do you want to add it? Y or N"
+        if ($input -eq "Y"){Copy-Item $chromeSh -Destination $desktopPath}
+    }
+
+if ($desktopContents.Contains($edge)){
+    $input = Read-Host "There is an Edge desktop shortcut. Do you want to remove it? Y or N"
+    if ($input -eq "Y"){Remove-Item -Path $edgeDesk}
+    }else{
+        $input = Read-Host "There is no Edge desktop shortcut. Do you want to add it? Y or N"
+        if ($input -eq "Y"){Copy-Item $edgeSh -Destination $desktopPath}
+    }
+    
